157050213d14f02a1f82ecd48e22962eda4405fe
ltasneem <lubaba070@gmail.com>
2017-06-20 09:42:39 -0400

    fizzbuzz with bonus

---
+++ src/Fizz_buzz.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Fizz_buzz {
+	
+	public static void main(String[] args) {
+		
+		int fizzy=0,buzzy=0,fizzybuzzy=0;
+	
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println(i);
+			if(i%3==0 && i%5!=0)
+			{
+				fizzy+=1;
+				if(fizzy == 3)
+				{
+					fizzy=0;
+					System.out.println("Fizzy");
+				}
+				else
+				{
+					System.out.println("Fizz");
+				}
+			}
+			else if(i%5==0 && i%3!=0)
+			{
+				buzzy+=1;
+				if(buzzy == 3)
+				{
+					buzzy =0;
+					System.out.println("Buzzy");
+				}
+				else
+				{
+					System.out.println("Buzz");
+				}
+			}
+			else if(i%5==0 && i%3==0)
+			{
+				fizzybuzzy+=1;
+				if(fizzybuzzy == 3)
+				{
+					fizzybuzzy =0;
+					System.out.println("FizzyBuzzy");
+				}
+				else
+				{
+					System.out.println("FizzBuzz");
+				}
+			}
+		}
+	}
+
+}
